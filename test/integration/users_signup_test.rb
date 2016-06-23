333ed9952dc2f49f022c44a829e614cb7babf37b
hrfmmymt <hrfmmymt@gmail.com>
2016-06-24 01:50:20 +0900

    テストコケる

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -22,5 +22,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
 end
