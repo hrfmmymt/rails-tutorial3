ac4caf8731467a3433f0a7cd1a13a054380e261e
hrfmmymt <hrfmmymt@gmail.com>
2016-07-11 19:16:40 +0900

    modified.

--- app/models/user.rb
+++ app/models/user.rb
@@ -58,8 +58,13 @@ class User < ActiveRecord::Base
   end
 
   # パスワード再設定のメールを送信する
-  def send_password_reset_mail
+  def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
+  end
+
+  # パスワード再設定の期限が切れている場合はtrueを返す
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
